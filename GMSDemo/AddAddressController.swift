b982c93e1ab48b5d796ea82feaa339e970331c69
Rey Cerio <ceriosrey@gmail.com>
2018-06-18 22:37:03 -0700

    first commit

--- GMSDemo/AddAddressController.swift
+++ GMSDemo/AddAddressController.swift
@@ -6,4 +6,83 @@
 //  Copyright © 2018 Rey Cerio. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AddAddressController: UIViewController {
+    
+    
+    let addressTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "enter address..."
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let instructionLabel : UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Instructions: Enter Address in this order: apartment, street, city,province/state, country."
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let lineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupViews()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(handleBackTap))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Submit", style: .plain, target: self, action: #selector(handleSubmit))
+    }
+    
+    private func setupViews() {
+        view.addSubview(addressTextField)
+        view.addSubview(lineView)
+        view.addSubview(instructionLabel)
+        
+        addressTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+        addressTextField.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
+        addressTextField.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 10).isActive = true
+        addressTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        lineView.topAnchor.constraint(equalTo: addressTextField.bottomAnchor).isActive = true
+        lineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        lineView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
+        lineView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 10).isActive = true
+        
+        instructionLabel.topAnchor.constraint(equalTo: addressTextField.bottomAnchor, constant: 8).isActive = true
+        instructionLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
+        instructionLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 10).isActive = true
+        instructionLabel.heightAnchor.constraint(equalToConstant: 60).isActive = true
+    }
+    
+    @objc func handleSubmit() {
+        guard let location = self.addressTextField.text else {
+            //alert here stating address textfield is empty
+            return
+        }
+        presentMapView(location: location)
+    }
+    
+    @objc func handleBackTap() {
+        guard let location = self.addressTextField.text else {
+            //alert here stating address textfield is empty
+            return
+        }
+        presentMapView(location: location)
+    }
+    
+    private func presentMapView(location: String) {
+        let mapViewController = MapViewController()
+        mapViewController.addressLocation = location
+        let mapViewNav = UINavigationController(rootViewController: mapViewController)
+        self.present(mapViewNav, animated: true, completion: nil)
+    }
+}
--- GMSDemo/MapViewController.swift
+++ GMSDemo/MapViewController.swift
@@ -11,43 +11,87 @@ import GoogleMaps
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
     
-    var gmsMapView : GMSMapView?
+    var gmsMapView: GMSMapView?
     let locationManager = CLLocationManager()
+    var userLocation : CLLocationCoordinate2D?
+    var addressLocation: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(seguesToAddAddressController))
         GMSServices.provideAPIKey("AIzaSyAOhiBw8mSPBmmAJQ_fjM79x7ruvMxFmxQ")
+        
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-//        let camera = GMSCameraPosition.camera(withLatitude: 37.621262, longitude: -122.378945, zoom: 12)
-//        gmsMapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-//        view = gmsMapView
-        
-//        let currentLocation = CLLocationCoordinate2D(latitude: 37.621262, longitude: -122.378945)
-//        let marker = GMSMarker(position: currentLocation)
-//        marker.title = "SFO Airport"
-//        marker.map = gmsMapView
-        
+        setupGMSMapView()
+
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
         let location = locations[0]
         let center = location.coordinate
-        let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
-        let region = MKCoordinateRegion(center: center, span: span)
-        
+        userLocation = center
         let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: 18)
-        gmsMapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = gmsMapView
+        gmsMapView?.animate(to: camera)
+        gmsMapView?.isMyLocationEnabled = true
+
+        //DO NOT USE THIS, INSTEAD MAKE A STATIC MAP
+//        gmsMapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+//        view = gmsMapView
         
-        let marker = GMSMarker(position: location.coordinate)
-        marker.title = "Me!"
-        marker.map = gmsMapView
+        if addressLocation != "" || addressLocation != nil {
+            if let address = addressLocation {
+                var coordinates = CLLocationCoordinate2D()
+                let geocoder = CLGeocoder()
+                
+                geocoder.geocodeAddressString(address) { (placemarks, error) in
+                    if error != nil {
+                        //put an alert here for no placemarks found
+                        print("GEOCODER: No Placemarks Found!")
+                    } else {
+                        guard let unwrappedCoord = placemarks?.first?.location?.coordinate else {return}
+                        coordinates = unwrappedCoord
+                        let userPoint = MKMapPointForCoordinate(center)
+                        let addressPoint = MKMapPointForCoordinate(coordinates)
+                        let distance = MKMetersBetweenMapPoints(userPoint, addressPoint)
+                        if distance < 100000 {
+                            let newMarker = GMSMarker(position: coordinates)
+                            newMarker.title = address
+                            newMarker.map = self.gmsMapView
+                            self.gmsMapView?.animate(toLocation: coordinates)
+                            self.gmsMapView?.isMyLocationEnabled = true
+                        }
+                    }
+                }
+                addressLocation = nil
+            }
+        }
+    }
+    
+    private func setupGMSMapView() {
+        
+        //make the map static
+        //do not use view = googlemap
+        
+        gmsMapView = GMSMapView()
+        gmsMapView?.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(gmsMapView!)
+        gmsMapView?.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        gmsMapView?.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        gmsMapView?.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        gmsMapView?.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+
+    }
+    
+    @objc func seguesToAddAddressController() {
+        let addAddress = AddAddressController()
+        let addAddressNav = UINavigationController(rootViewController: addAddress)
+        self.present(addAddressNav, animated: true) {
+        }
     }
 
 }
